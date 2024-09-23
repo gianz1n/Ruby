6c3c5a4b08cf4e38ed95d142eaccee2d81130c3e
Gian Emanuel da Silva <105940828+gianz1n@users.noreply.github.com>
2024-09-23 13:50:12 -0300

    Estrutura de Repetição

---
+++ codigos_aprendizado/estrutura_de_repeticao/for.rb
@@ -0,0 +1,8 @@
+frutas = ['Morango', 'Uva', 'Laranja']
+contador = 1
+
+for i in frutas
+    print "#{contador}. "
+    puts i 
+    contador = contador + 1
+end
---
+++ codigos_aprendizado/estrutura_de_repeticao/loop.rb
@@ -0,0 +1,7 @@
+cont = 1 
+
+loop do
+    print "#{cont}.. "
+    break if cont == 100
+cont += 1
+end
