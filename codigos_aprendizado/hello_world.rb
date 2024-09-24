3549ba1383cb3ca3005342f24dd843cc72a18af0
Gian Emanuel da Silva <105940828+gianz1n@users.noreply.github.com>
2024-09-24 13:10:21 -0300

    Hello World

--- codigos_aprendizado/hello_world.rb
+++ codigos_aprendizado/hello_world.rb
@@ -1,5 +1,5 @@
 # puts é o comando para mostrar no terminal com quebra de linhas.
 # ferramenta para codar sem criar o arquivo: cmd.exe > irb
 
-puts "Hello, world!"
-puts 2*2    
+print "Hello, World!"
+  
